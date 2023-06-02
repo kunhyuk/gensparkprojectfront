960a4007def563843dd6f8cb8cf0ec11f795a66f
Tyler Nenninger <tynenninger@gmail.com>
2023-06-02 14:55:59 -0500

    auth working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CognitoService } from './cognito.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gensparkProjectFront';
+
+  constructor(private cognitoService: CognitoService) {}
+
+  public isAuthenticated(): boolean {
+    return this.cognitoService.isAuthenticated();
+  }
 }
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,17 +4,24 @@ import { CardListComponent } from './card-list/card-list.component';
 import { CreateCardComponent } from './create-card/create-card.component';
 import { UpdateCardComponent } from './update-card/update-card.component';
 import { CardDetailsComponent } from './card-details/card-details.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LogoutComponent } from './logout/logout.component'
+import { RouteGuardService } from './route-guard.service'
 
 const routes: Routes = [
-  {path: 'cards', component: CardListComponent},
-  {path: 'create-card', component: CreateCardComponent},
+  {path: 'cards', component: CardListComponent, canActivate: [RouteGuardService]},
+  {path: 'create-card', component: CreateCardComponent, canActivate: [RouteGuardService]},
   {path: '', redirectTo: 'cards', pathMatch: 'full'},
-  {path: 'update-card/:id', component: UpdateCardComponent},
-  {path: 'card-details/:id', component: CardDetailsComponent}
+  {path: 'update-card/:id', component: UpdateCardComponent, canActivate: [RouteGuardService]},
+  {path: 'card-details/:id', component: CardDetailsComponent, canActivate: [RouteGuardService]},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'logout', component: LogoutComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],                                                                                                                                                                                                                                                                                                          
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
